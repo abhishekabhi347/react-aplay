656614e8ae99f8c32fb9b854be4ff2fa5c577275
abhishek <arampi@issi-software.com>
2019-11-28 19:01:49 +0530

    new tasks

--- src/components/newtasks.js
+++ src/components/newtasks.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 const NewTasks = () => {
-  return <div>NewTasks</div>;
+  const [formdata, setFormData] = useState({
+    tasks: []
+  });
+
+  const { tasks } = formdata;
+
+  const onSubmit = e => {
+    e.preventDefault();
+    alert(formdata);
+  };
+  return (
+    <Fragment>
+      <from
+        name="frmnewtask"
+        onSubmit={e => {
+          onSubmit(e);
+        }}
+      >
+        {tasks.length > 0 ? <p>tasks</p> : <p>nill</p>}
+        <div>
+          <textarea
+            name="txtnewtask"
+            id="txtnewtask"
+            style={{ resize: "none" }}
+            rows="4"
+          ></textarea>
+          <button type="submit" name="btnAddNew" id="btnAddNew">
+            Add
+          </button>
+          &nbsp;
+          <button type="button" name="btnRemove" id="btnRemove">
+            Remove
+          </button>
+        </div>
+      </from>
+    </Fragment>
+  );
 };
 
 export default NewTasks;
